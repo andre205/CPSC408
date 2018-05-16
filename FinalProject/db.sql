c23c03377f52730b2c3fcd5dd9d6b17e90eec0a0
andre205 <andre205@mail.chapman.edu>
2018-05-15 22:43:53 -0700

    schema update

--- FinalProject/db.sql
+++ FinalProject/db.sql
@@ -1,47 +1,34 @@
 -- auto-generated definition
-CREATE TABLE Admins
-(
-  userID     INT AUTO_INCREMENT
-    PRIMARY KEY,
-  adminLevel INT DEFAULT '1' NOT NULL,
-  CONSTRAINT Admins_userID_uindex
-  UNIQUE (userID),
-  CONSTRAINT Admins_Users_userID_fk
-  FOREIGN KEY (userID) REFERENCES flaskdb.Users (userID)
-)
-  ENGINE = InnoDB;
+CREATE TABLE `admins` (
+  `userID` int(11) NOT NULL AUTO_INCREMENT,
+  `adminLevel` int(11) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`userID`),
+  UNIQUE KEY `Admins_userID_uindex` (`userID`),
+  CONSTRAINT `Admins_Users_userID_fk` FOREIGN KEY (`userID`) REFERENCES `users` (`userid`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
 
-  -- auto-generated definition
-  CREATE TABLE Users
-  (
-    userID      INT AUTO_INCREMENT
-      PRIMARY KEY,
-    username    VARCHAR(20) NOT NULL,
-    pass        VARCHAR(50) NOT NULL,
-    firstName   VARCHAR(50) NULL,
-    lastName    VARCHAR(50) NULL,
-    dateCreated DATE        NULL,
-    CONSTRAINT Users_userID_uindex
-    UNIQUE (userID),
-    CONSTRAINT Users_username_uindex
-    UNIQUE (username)
-  )
-    ENGINE = InnoDB;
 
--- auto-generated definition
-CREATE TABLE Posts
-(
-  postID    INT AUTO_INCREMENT
-    PRIMARY KEY,
-  userID    INT                                 NOT NULL,
-  message   VARCHAR(1000)                       NULL,
-  timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL ON UPDATE CURRENT_TIMESTAMP,
-  CONSTRAINT Posts_postID_uindex
-  UNIQUE (postID),
-  CONSTRAINT Posts_Users_userID_fk
-  FOREIGN KEY (userID) REFERENCES flaskdb.Users (userID)
-)
-  ENGINE = InnoDB;
+CREATE TABLE `post` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `author_id` int(11) NOT NULL,
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `title` varchar(20) NOT NULL,
+  `body` varchar(500) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `author_id` (`author_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+
+CREATE TABLE `users` (
+  `userID` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(20) NOT NULL,
+  `pass` varchar(50) NOT NULL,
+  `firstName` varchar(50) DEFAULT NULL,
+  `lastName` varchar(50) DEFAULT NULL,
+  `dateCreated` date DEFAULT NULL,
+  PRIMARY KEY (`userID`),
+  UNIQUE KEY `Users_userID_uindex` (`userID`),
+  UNIQUE KEY `Users_username_uindex` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
 
 CREATE INDEX Posts_Users_userID_fk
   ON Posts (userID);
